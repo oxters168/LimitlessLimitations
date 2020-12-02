cbdd4d58218feb8f2c6590bf249583aa49421857
Oxters <oxters168@gmail.com>
2020-12-02 03:04:36 +0200

    Created a basic follow AI for humanoids. Made it possible for humanoids to collide with each other.

---
+++ Assets/Core/Scripts/HumanoidAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityHelpers;
+
+public class HumanoidAI : MonoBehaviour
+{
+    public Transform target;
+
+    private IValueManager _inputAI;
+    private IValueManager InputAI { get { if (_inputAI == null) _inputAI = GetComponent<IValueManager>(); return _inputAI; } }
+
+    void Update()
+    {
+        Vector2 input = Vector2.zero;
+        bool run = false;
+        if (target != null)
+        {
+            Vector2 diff = target.position.xz() - transform.position.xz();
+            float distance = diff.magnitude;
+            if (distance > 1)
+                run = true;
+            if (distance > 0.01f)
+                input = diff.normalized;
+            // input = input.Sign();
+        }
+
+        // if (!input.IsZero())
+        // {
+            InputAI.SetAxis("dpadHor", input.x);
+            InputAI.SetAxis("dpadVer", input.y);
+            InputAI.SetToggle("crossBtn", run);
+        // }
+    }
+}
